ce6fe61e04e769c70f7ae05d33530c9df06c1150
zxkws <loveyhcui@163.com>
2025-01-23 19:33:33 +0800

    移除无用代码

--- packages/frontend/src/components/Menu.tsx
+++ packages/frontend/src/components/Menu.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Layout, Menu as AntMenu } from "antd";
-import { CalendarOutlined, SettingOutlined, HomeOutlined, BarChartOutlined, UserOutlined } from "@ant-design/icons";
+import { CalendarOutlined, HomeOutlined, BarChartOutlined, UserOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
 const { Sider, Content } = Layout;
